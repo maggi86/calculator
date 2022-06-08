050da2c7b7549493ce0c57471f9af06e4971f44e
Meagan Williams <meaganwilliams407@gmail>
2022-06-08 16:12:10 +0200

    edit

---
+++ code.js
@@ -0,0 +1,14 @@
+switch(ops) {
+    case '+':
+        total = eval(fstOp.concat(ops,secOp));
+    break
+    case '-':
+        total = eval(fstOp.concat(ops,secOp));
+    break
+    case '*':
+        total = eval(fstOp.concat(ops,secOp));
+    break
+    case '/':
+        total = eval(fstOp.concat(ops,secOp));
+    break
+}
